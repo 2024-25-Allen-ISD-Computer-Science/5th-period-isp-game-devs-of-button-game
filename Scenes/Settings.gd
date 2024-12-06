f6f622234557e68520a5509c0eae1f7b83d37065
TMS-01 <142348612+TMS-01@users.noreply.github.com>
2024-12-05 23:15:49 -0600

    Add files via upload

---
+++ Scenes/Settings.gd
@@ -0,0 +1,19 @@
+extends Button
+
+
+func _ready():
+	$"../../VBoxContainer/music slider".value = db_to_linear(AudioServer.get_bus_volume_db(0))
+
+#exit settings
+func _on_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/title_screen.tscn")
+
+
+func _on_h_slider_mouse_exited() -> void:
+	release_focus()
+
+
+
+
+func _on_confirm_settings_pressed() -> void:
+	AudioServer.set_bus_volume_db(0, linear_to_db($"../../VBoxContainer/music slider".value))
