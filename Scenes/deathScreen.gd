8b93f888357520be1486d323493fca47855d1efc
TMS-01 <142348612+TMS-01@users.noreply.github.com>
2025-01-31 00:37:19 -0600

    deathScreen.gd

--- Scenes/deathScreen.gd
+++ Scenes/deathScreen.gd
@@ -9,3 +9,12 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+var player_health = 100
+func _health(death):
+	if player_health <= 0:
+		get_tree().change_scene_to_file("res://deathScreen.gd")
+func _display() -> void:
+	print("You died loser")
+func _on_pressed() -> void:
+	get_tree().change_scene_to_file("res://title_screen.tscn")
