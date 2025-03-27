2099580a51e219c0d5cf7a5b077ff3d7c54ef092
Vidit <111073793+viditsmart@users.noreply.github.com>
2025-03-27 06:53:40 -0500

    Made changes to this file
    I made some changes to this file, but there are some bugs I need to fix.

--- NewButtonMazeEvent.gd
+++ NewButtonMazeEvent.gd
@@ -1,8 +1,8 @@
 extends Node3D
 
-@onready var player = $Node3D/CharacterBody3D
-@onready var electric_orb_container = $Node3D/ElectricOrbContainer
-signal orb_collected
+@onready var player = $GridMap/CharacterBody3D
+@onready var electric_orb_container = $GridMap/ElectricOrbContainer
+#signal orb_collected
 
 var collected_orbs = 0
 var total_orb_count = 0
@@ -11,9 +11,9 @@ var total_orb_count = 0
 func _ready() -> void:
 	total_orb_count = electric_orb_container.get_child_count()
 	
-	player.connect("orb_collected", self, "on_orb_collected")
+	player.orb_collected.connect(_on_orb_collected)
 
-func on_orb_collected():
+func _on_orb_collected():
 	collected_orbs += 1
 	if collected_orbs >= total_orb_count:
 		emit_signal("orb_collected")
